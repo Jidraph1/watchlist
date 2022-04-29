1a2f9d2ab7dfae390b2ba40b16acc38f598552e2
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-04-30 01:01:48 +0300

    deployment to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,12 @@
-from flask import render_template, request, redirect, url_for
-from app import app
-from .request import get_movies,get_movie, search_movie
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_movies,get_movie,search_movie
+from .forms import ReviewForm
+from ..models import Review
+Review = review.Review
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -24,15 +27,8 @@ def index():
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
-# @app.route('/movie/<int:movie_id>')
-# def movie(movie_id):
 
-#     '''
-#     View movie page function that returns the movie details page and its data
-#     '''
-#     return render_template('movie.html',id = movie_id)
-
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -40,10 +36,11 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
 
     return render_template('movie.html',title = title,movie = movie)
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -53,3 +50,18 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY= 'e1acde443209454ddafb61583da42656'
+export SECRET_KEY= 'askjdlaskjfiojfkasmlfkl'
+
+python3.8 manage.py server
